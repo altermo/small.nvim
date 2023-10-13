197d4202ff1419901e8e3145962e1f0119d426b6
altermo <>
2023-10-13 15:48:00 +0200

    M lua/small/.not_done/cursor.lua

--- lua/small/.not_done/cursor.lua
+++ lua/small/.not_done/cursor.lua
@@ -9,38 +9,66 @@ A temp cursor: cursor which is temporary and will be deleted once not focused (c
     spawner + temp : a mark system
 Option to hide cursor/ make it look different
     Different colors for different modes
+Have a main cursor object (which is auto created?)
 --]]
-local M={conf={ns=nil}}
-function M.get_ns()
-    return M.conf.ns or vim.api.nvim_create_namespace('small_cursor')
+---@class cursor.group
+---@field content cursor.cursor[]
+---@field namespace number
+---@class cursor.cursor
+---@field mode 'normal'|'_Error'
+---@field buffer number
+---@field _extmark number
+---@field highlight? cursor.highlight
+---@field type 'normal'|'temp'|'spawner'
+---@class cursor.highlight
+---@field hidden? boolean
+---@field mode_hl? table<string,string>
+---@field select_hl? string
+
+local M={I={}}
+---@param pos? number[]|string
+---@return number[]
+function M.I.get_pos(pos)
+    if type(pos)=='table' then return pos end
+    local gpos=vim.fn.getpos(pos or '.')
+    return {gpos[2]-1,gpos[3]-1+gpos[4]}
 end
+---@param g cursor.group
+---@param position? string|number[]
 ---@param buf? number
----@return number[][]
-function M.get_cursors_ext(buf)
-    local ns=M.get_ns()
-    return vim.api.nvim_buf_get_extmarks(buf or 0,ns,0,-1,{details=true})
-end
----@param pos? string|number[]
----@param buf? number
----@return number?
----@return number?
-function M.create_cursor(pos,buf)
-    local ns=M.get_ns()
-    if type(pos)~='table' then
-        local gpos=vim.fn.getpos(pos or '.')
-        pos={gpos[2]-1,gpos[3]-1+gpos[4]}
-    end
-    for _,v in ipairs(M.get_cursors_ext(buf)) do
-        if v[2]==pos[1] and v[3]==pos[2] then
-            return nil,v[1]
-        end
-    end
-    return vim.api.nvim_buf_set_extmark(buf or 0,ns,pos[1],pos[2],{
-        hl_group='Cursor',end_col=pos[2]+1,
+function M.create_cursor(g,position,buf)
+    local pos=M.I.get_pos(position)
+    table.insert(g.content,{
+        mode='normal',
+        buffer=buf or 0,
+        _extmark=vim.api.nvim_buf_set_extmark(buf or 0,g.namespace,pos[1],pos[2],{hl_group='Cursor',end_col=pos[2]+1,}),
+        type='normal',
     })
 end
+---@param g cursor.group
+---@param cur cursor.cursor
+function M.jump_to_cursor(g,cur)
+    M.create_cursor(g)
+    local ext=vim.api.nvim_buf_get_extmark_by_id(cur.buffer,g.namespace,cur._extmark,{})
+    vim.api.nvim_win_set_cursor(0,{ext[1]+1,ext[2]})
+    vim.api.nvim_buf_del_extmark(cur.buffer,g.namespace,cur._extmark)
+    for k,v in ipairs(g.content) do
+        if v==cur then table.remove(g.content,k) break end
+    end
+end
+---@param ns? string
+---@return cursor.group
+function M.create_cursor_group(ns)
+    return {
+        namespace=ns or vim.api.nvim_create_namespace(''),
+        content={},
+    }
+end
 if vim.dev then
-    M.create_cursor()
-    vim.pprint(M.get_cursors_ext(0))
+    if _G.Group then vim.api.nvim_buf_clear_namespace(0,_G.Group.namespace,0,-1) end
+    local g=M.create_cursor_group()
+    M.create_cursor(g)
+    vim.keymap.set('n','<F12>',function() M.jump_to_cursor(g,g.content[1]) end)
+    _G.Group=g
 end
 return M
