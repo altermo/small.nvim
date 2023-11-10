4f4ad3234ae0b37200bab68ec6b7611c42dcc88a
altermo <>
2023-11-10 15:36:33 +0100

    A  lua/small/emacs/recenter_top_bottom.lua

---
+++ lua/small/emacs/recenter_top_bottom.lua
@@ -0,0 +1,10 @@
+local M={current=0}
+return function ()
+  M.current=M.current%3+1
+  if M.current==3 then vim.cmd.norm{'zb',bang=true}
+  elseif M.current==2 then vim.cmd.norm{'zt',bang=true}
+  else
+    vim.cmd.norm{'zz',bang=true}
+    vim.api.nvim_create_autocmd({'CursorMoved','CursorMovedI'},{once=true,callback=function() M.current=0 end,group=vim.api.nvim_create_augroup('Cz',{clear=true})})
+  end
+end,M
