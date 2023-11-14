b425598851d1eeb9e973f310517ff331482ea8e0
altermo <>
2023-11-14 11:37:05 +0100

    M lua/small/notify.lua

--- lua/small/notify.lua
+++ lua/small/notify.lua
@@ -27,14 +27,18 @@ end
 function M.overide_notify()
     rawset(vim,'notify',M.notify)
 end
-function M.update()
+M.update=function()
     local i=0
     for k,v in vim.spairs(M.notifs) do
         if v.dispand then
             if vim.api.nvim_get_current_buf()==v.buf then
                 vim.api.nvim_create_autocmd('WinLeave',{buffer=v.buf,callback=vim.schedule_wrap(M.update),once=true})
             else
-                if v.buf then vim.api.nvim_buf_delete(v.buf,{force=true}) end
+                if v.buf then
+                    if not pcall(vim.api.nvim_buf_delete,v.buf,{force=true}) then
+                        vim.defer_fn(M.update,10) return
+                    end
+                end
                 M.notifs[k]=nil
             end
             goto continue
@@ -50,14 +54,21 @@ function M.update()
             row=i,
         }
         i=i+#text
-        if not v.buf then
-            v.buf=vim.api.nvim_create_buf(false,true)
-            vim.api.nvim_buf_set_lines(v.buf,0,-1,false,text)
+        if not v.hasbuf then
+            v.buf=v.buf or vim.api.nvim_create_buf(false,true)
+            if not pcall(vim.api.nvim_buf_set_lines,v.buf,0,-1,false,text) then
+                vim.defer_fn(M.update,10) return
+            end
+            v.hasbuf=true
         end
         if not v.win then
             local style=M.conf.style[level] or {hl='Normal'}
             if type(style)=='string' then style={hl=style} end
-            v.win=vim.api.nvim_open_win(v.buf,false,vim.tbl_extend('force',{style='minimal',noautocmd=true},winopt))
+            local s,win=pcall(vim.api.nvim_open_win,v.buf,false,vim.tbl_extend('force',{style='minimal',noautocmd=true},winopt))
+            if not s then
+                vim.defer_fn(M.update,10) return
+            end
+            v.win=win
             vim.wo[v.win].winblend=style.winblend or 100
             vim.wo[v.win].winhl='Normal:'..style.hl
         else
