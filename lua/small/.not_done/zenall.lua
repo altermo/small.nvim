85b09f9e7f2d742858a82a16dc07973a31ad313a
altermo <>
2023-12-03 12:29:54 +0100

    M lua/small/.not_done/zenall.lua

--- lua/small/.not_done/zenall.lua
+++ lua/small/.not_done/zenall.lua
@@ -1,12 +1,15 @@
 local M={ns=vim.api.nvim_create_namespace'small_zenall'}
 function M.run()
-    M.zen_windows={}
     vim.api.nvim_set_decoration_provider(M.ns,{
-        on_win=function (_,winid)
-            return M.zen_windows[winid]
+        on_win=function (_,_)
+            return true
         end,
-        on_line=function (_,winid,bufnr,row)
-            vim.lg(winid,bufnr,row)
+        on_line=function (_,_,bufnr,row)
+            vim.api.nvim_buf_set_extmark(bufnr,M.ns,row,0,{
+                ephemeral=true,
+                virt_text_pos='inline',
+                virt_text={{'  '}},
+            })
         end
     })
 end
