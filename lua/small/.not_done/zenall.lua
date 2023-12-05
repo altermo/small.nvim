eae164171849bd5aca4f8ddc8f4c74f64795198a
altermo <>
2023-12-05 10:27:25 +0100

    M lua/small/.not_done/zenall.lua; M lua/small/.not_done/zenall_.lua

--- lua/small/.not_done/zenall.lua
+++ lua/small/.not_done/zenall.lua
@@ -1,30 +1,43 @@
----a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong line
 local M={
     ns=vim.api.nvim_create_namespace'small_zenall',
-    buffers={},
+    augroup=vim.api.nvim_create_augroup('small_zenall',{}),
 }
 function M.redraw(buf)
-    local zen='   '
-    vim.api.nvim_buf_clear_namespace(buf,M.ns,0,-1)
-    local win=vim.fn.win_findbuf(buf)[1]
-    vim.wo[win].statuscolumn=zen
-    local view=vim.fn.winsaveview()
-    local width=vim.api.nvim_win_get_width(win)
-    for row=0,vim.api.nvim_buf_line_count(buf) do
-        pcall(vim.api.nvim_buf_set_extmark,buf,M.ns,row,width+view.leftcol-#zen*2,{
-            virt_text_pos='inline',
-            virt_text={{zen}},
-        })
-    end
+    _=buf
 end
-function M.run(buf)
-    buf=vim.fn.bufnr(buf)
-    local win=vim.fn.win_findbuf(buf)[1]
-    vim.wo[win].showbreak=''
-    vim.wo[win].wrap=false
-    vim.wo[win].cursorline=false
-    vim.wo[win].signcolumn='no'
-    local function redraw() M.redraw(buf) end
+function M.wininit(win)
+    if not vim.b[vim.api.nvim_win_get_buf(win)].small_zenall_enabled then return end
+    if vim.w[win].small_zenall_enabled then return end
+    local save={}
+    for k,v in pairs({
+        showbreak='',
+        wrap=false,
+        cursorline=false,
+        signcolumn='no',
+        statuscolumn='',
+    }) do
+        save[k]=vim.wo[win][k]
+        vim.wo[win][k]=v
+    end
+    vim.w[win].small_zenall_enabled=save
+end
+function M.windeinit(win)
+    if vim.b[vim.api.nvim_win_get_buf(win)].small_zenall_enabled then return end
+    if not vim.w[win].small_zenall_enabled then return end
+    for k,v in pairs(vim.w[win].small_zenall_enabled) do
+        vim.wo[win][k]=v
+    end
+    vim.w[win].small_zenall_enabled=nil
+end
+function M.setup()
+    if M.hassetup then return end
+    M.hassetup=true
+    local function redraw(ev)
+        M.redraw(ev.buf)
+        vim.schedule_wrap(M.redraw)(ev.buf)
+    end
+    local function wininit() M.wininit(vim.api.nvim_get_current_win()) end
+    local function windeinit() M.windeinit(vim.api.nvim_get_current_win()) end
     vim.api.nvim_create_autocmd({
         'CursorMoved',
         'CursorMovedI',
@@ -32,15 +45,28 @@ function M.run(buf)
         'TextChangedI',
         'TextChangedP',
         'WinResized',
-        'WinNew',
-        'WinLeave',
-    },{
-            buffer=buf,
-            callback=redraw
-        })
+    },{callback=redraw,group=M.augroup})
+    vim.api.nvim_create_autocmd({
+        'BufWinEnter',
+        'WinEnter',
+    },{callback=wininit,group=M.augroup})
+    vim.api.nvim_create_autocmd({
+        'BufWinLeave',
+        'BufLeave',
+        'BufEnter'
+    },{callback=windeinit,group=M.augroup})
+end
+function M.run(buf)
+    if vim.b[buf].small_zenall_enabled then return end
+    vim.b[buf].small_zenall_enabled=true
+    M.setup()
     M.redraw(buf)
+    M.wininit(vim.api.nvim_get_current_win())
 end
 if vim.dev then
+    vim.b[vim.api.nvim_get_current_buf()].small_zenall_enabled=false
+    vim.api.nvim_del_augroup_by_id(M.augroup)
+    M.augroup=vim.api.nvim_create_augroup('small_zenall',{})
+    M.hassetup=false
     M.run(0)
 end
-return M
--- lua/small/.not_done/zenall_.lua
+++ lua/small/.not_done/zenall_.lua
@@ -1,79 +1,53 @@
-local M={buf=vim.api.nvim_create_buf(false,true)}
-function M.set_win(win)
-    vim.wo[win].number=false
-    vim.wo[win].relativenumber=false
+local M={
+    ns=vim.api.nvim_create_namespace'small_zenall',
+    buffers={},
+}
+function M.redraw(buf)
+    vim.api.nvim_buf_clear_namespace(buf,M.ns,0,-1)
+    local win=vim.fn.win_findbuf(buf)[1]
+    vim.wo[win].showbreak=''
+    vim.wo[win].wrap=false
     vim.wo[win].cursorline=false
-    vim.wo[win].colorcolumn=''
     vim.wo[win].signcolumn='no'
-end
-function M.make_right_win(s)
-    vim.cmd'rightbelow vsplit'
-    vim.api.nvim_set_current_buf(M.buf)
-    s.right=vim.api.nvim_get_current_win()
-    M.set_win(s.right)
-    vim.cmd.wincmd'p'
-end
-function M.make_left_win(s)
-    vim.cmd'leftabove vsplit'
-    vim.api.nvim_set_current_buf(M.buf)
-    s.left=vim.api.nvim_get_current_win()
-    M.set_win(s.left)
-    vim.cmd.wincmd'p'
-end
-function M.make_win_zen(s)
-    local saveopt=vim.o.equalalways
-    local savewin=vim.api.nvim_get_current_win()
-    vim.api.nvim_set_current_win(s.win)
-    vim.o.equalalways=false
-    M.make_left_win(s)
-    M.make_right_win(s)
-    M.savef[s.left]=s
-    M.savef[s.right]=s
-    vim.api.nvim_set_current_win(savewin)
-    vim.o.equalalways=saveopt
-end
-function M.resize_all()
-    local s=vim.tbl_keys(M.save)
-    table.sort(s,function(a,b) return vim.api.nvim_win_get_position(a)[2]<vim.api.nvim_win_get_position(b)[2] end)
-    for _,v in ipairs(s) do
-        local m=M.save[v]
-        vim.api.nvim_win_set_width(m.left,math.floor((m.width-math.floor(m.width/10*8))/2))
-        vim.api.nvim_win_set_width(m.win,math.floor(m.width/10*8))
-        vim.api.nvim_win_set_width(m.right,m.width-math.floor((m.width-math.floor(m.width/10*8))/2)-math.floor(m.width/10*8))
+    local view=vim.fn.winsaveview()
+    local width=vim.api.nvim_win_get_width(win)
+    local zen=(' '):rep(vim.fn.floor(width/8))
+    vim.wo[win].statuscolumn=''
+    for row=0,vim.api.nvim_buf_line_count(buf) do
+        pcall(vim.api.nvim_buf_set_extmark,buf,M.ns,row,view.leftcol,{
+            virt_text_pos='inline',
+            virt_text={{zen}},
+        })
+        pcall(vim.api.nvim_buf_set_extmark,buf,M.ns,row,width+view.leftcol-#zen-#zen,{
+            virt_text_pos='inline',
+            virt_text={{zen}},
+        })
     end
 end
-function M.run()
-    M.save={}
-    M.savef={}
-    for _,win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-        vim.api.nvim_set_current_win(win)
-        local width=vim.api.nvim_win_get_width(win)
-        local s={win=win,width=width}
-        M.save[win]=s
+function M.run(buf)
+    buf=buf==0 and vim.api.nvim_get_current_buf() or buf
+    local function redraw()
+        M.redraw(buf)
+        vim.schedule_wrap(M.redraw)(buf)
     end
-    for _,v in pairs(M.save) do
-        M.make_win_zen(v)
-    end
-    M.resize_all()
-    M.set_autocmds()
-end
-function M.derun()
-    if not M.save then return end
-    for _,v in pairs(M.save) do
-        vim.api.nvim_win_close(v.left,true)
-        vim.api.nvim_win_close(v.right,true)
-        vim.api.nvim_win_set_width(v.win,v.width)
-    end
-    M.save=nil
-end
-function M.set_autocmds()
-    local g=vim.api.nvim_create_augroup('ZenAll',{})
-    vim.api.nvim_create_autocmd('WinEnter',{callback=function () end,group=g})
-    vim.api.nvim_create_autocmd('WinClosed',{callback=function () end,group=g})
-    vim.api.nvim_create_autocmd('WinResized',{callback=function () end,group=g})
+    vim.api.nvim_create_autocmd({
+        'CursorMoved',
+        'CursorMovedI',
+        'TextChanged',
+        'TextChangedI',
+        'TextChangedP',
+        'WinResized',
+        'WinNew',
+        'WinLeave',
+        'BufWinEnter',
+    },{
+            buffer=buf,
+            callback=redraw,
+            group=vim.api.nvim_create_augroup('small_zenall',{}),
+        })
+    M.redraw(buf)
 end
 if vim.dev then
-    M.run()
-    --M.derun()
+    M.run(0)
 end
 return M
