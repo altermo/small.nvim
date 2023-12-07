a3393b60d03103ff17405256b6c56a4bff32dfc2
altermo <>
2023-12-07 10:41:41 +0100

    R  lua/small/.not_done/emacs_open_org.lua -> lua/small/emacs_features/open_org.lua

--- lua/small/emacs_features/open_org.lua
+++ lua/small/emacs_features/open_org.lua
@@ -2,9 +2,8 @@ local M={}
 --TODO: continue
 function M.setup()
     vim.api.nvim_create_autocmd({'Filetype'},{callback=function (args)
-        vim.api.nvim_win_set_buf(0,vim.api.nvim_create_buf(true,true))
-        vim.api.nvim_buf_delete(args.buf,{})
-    vim.fn.termopen('emacs '..args.file)
+        vim.fn.termopen({'emacs','-nw','--',args.file})
+        vim.api.nvim_buf_set_name(0,args.file)
     end,pattern='org',group=vim.api.nvim_create_augroup('small_emacs_org',{})})
 
 end
