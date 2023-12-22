051c7dcb635b71ce2f5d67e8e4ecb16139011f7b
altermo <>
2023-12-22 10:50:44 +0100

    M lua/small/kitty/image.lua

--- lua/small/kitty/image.lua
+++ lua/small/kitty/image.lua
@@ -9,9 +9,9 @@ function M.is_png(source)
     local fd=vim.uv.fs_open(source,'r',0)
     return fd and vim.uv.fs_read(fd,8,0)=='\x89PNG\r\n\x1a\n'
 end
-function M.send_png_packet(payload,last,first,width,height)
+function M.send_png_packet(payload,last,first,width,height,lcmd)
     M.write('\x1b_G')
-    local cmd={'m='..(last and '0' or '1')}
+    local cmd={'m='..(last and '0' or '1'),lcmd}
     if first then
         table.insert(cmd,'a=T')
         table.insert(cmd,'f=100')
@@ -29,7 +29,7 @@ function M.chunckify(str)
     end
     return ret
 end
-function M.render(source,x,y,width,height,win)
+function M.render_direct(source,x,y,width,height,win)
     if not M.is_png(source) then error('source is not a png') end
     if win then
         local row,col=unpack(vim.api.nvim_win_get_position(win))
@@ -38,11 +38,29 @@ function M.render(source,x,y,width,height,win)
     local fd=io.open(source,'r')
     if not fd then error('failed to open image file') end
     local data=vim.base64.encode(fd:read('*a'))
+    fd:close()
     local chunks=M.chunckify(data)
     for k,chunk in ipairs(chunks) do
         if x and y then M.write('\x1b['..y..';'..x..'H') end
         M.send_png_packet(chunk,k==#chunks,k==1,width,height)
     end
+end
+function M.render_file(source,x,y,width,height,win)
+    if not M.is_png(source) then error('source is not a png') end
+    if vim.fn.filereadable(source)==0 then error('source is not a file') end
+    if win then
+        local row,col=unpack(vim.api.nvim_win_get_position(win))
+        y,x=y+row,x+col
+    end
+    local data=vim.base64.encode(source)
+    local chunks=M.chunckify(data)
+    for k,chunk in ipairs(chunks) do
+        if x and y then M.write('\x1b['..y..';'..x..'H') end
+        M.send_png_packet(chunk,k==#chunks,k==1,width,height,'t=f')
+    end
+end
+function M.render(...)
+    M.render_file(...)
 end
 function M.clear()
     M.write('\x1b_Ga=d\x1b\\')
