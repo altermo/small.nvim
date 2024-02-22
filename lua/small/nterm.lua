a571f888c5fa710c927aecfe96a0af3668b2f112
altermo <107814000+altermo@users.noreply.github.com>
2024-02-22 13:46:35 +0100

    M lua/small/nterm.lua

--- lua/small/nterm.lua
+++ lua/small/nterm.lua
@@ -6,7 +6,10 @@ function M.run(cmd,smart_quit_nvim)
     vim.cmd.enew()
     local buf=vim.api.nvim_get_current_buf()
     vim.fn.termopen(cmd,{on_exit=function (_,_,_)
-        if smart_quit_nvim and #vim.fn.getbufinfo()==1 and vim.api.nvim_get_current_buf()==buf then vim.cmd.quitall() end
+        if smart_quit_nvim and #vim.tbl_filter(function (b)
+            return vim.bo[b].buftype~='nofile'
+        end,vim.api.nvim_list_bufs())==1 and vim.api.nvim_get_current_buf()==buf and buf==1
+        then vim.cmd.quitall() end
         pcall(vim.cmd.bdelete,{buf,bang=true})
     end})
     vim.cmd.startinsert()
