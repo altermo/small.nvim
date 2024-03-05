9eab0c9886a079611ccf6126bced37eb2163163d
altermo <107814000+altermo@users.noreply.github.com>
2024-03-05 13:14:13 +0100

    M  lua/small/.not_done/statusbuf.lua

--- lua/small/.not_done/statusbuf.lua
+++ lua/small/.not_done/statusbuf.lua
@@ -1,54 +1,70 @@
 local M={}
----@param opt? vim.api.keyset.eval_statusline
----@param statusline? string
----@return string[][]
-function M.get_statusline(opt,statusline)
-    opt=vim.tbl_extend('force',{highlights=true},opt)
-    local s=vim.api.nvim_eval_statusline(statusline or vim.o.statusline,opt)
-    local str=s.str
-    local high=s.highlights
-    local out={}
-    for k,v in ipairs(high) do
-        local next=high[k+1] or {start=#str}
-        table.insert(out,{str:sub(v.start+1,next.start),v.group})
+M.ns=vim.api.nvim_create_namespace'small_statusbuf'
+-----@param opt? vim.api.keyset.eval_statusline
+-----@param statusline? string
+-----@return string[][]
+--function M.get_statusline(opt,statusline)
+--opt=vim.tbl_extend('force',{highlights=true},opt)
+--local s=vim.api.nvim_eval_statusline(statusline or vim.o.statusline,opt)
+--local str=s.str
+--local high=s.highlights
+--local out={}
+--for k,v in ipairs(high) do
+--local next=high[k+1] or {start=#str}
+--table.insert(out,{str:sub(v.start+1,next.start),v.group})
+--end
+--return out
+--end
+--function M.replace_statusline()
+--if vim.o.laststatus~=1  then
+--error('\nWill not work if laststatus is not 1 (setting laststatus to 1)\nNote: if you change laststatus while it is active then it may break')
+--end
+--vim.o.laststatus=1
+--local buf=vim.api.nvim_create_buf(false,true)
+--local win=vim.api.nvim_open_win(buf,false,{
+--relative='win',
+--row=vim.o.lines-1,col=1,
+--width=vim.o.columns,height=1,
+--style='minimal'
+--})
+--local update=vim.schedule_wrap(function ()
+--local statusline=vim.o.statusline
+--if vim.o.statusline==' ' then return end
+--vim.o.statusline=' '
+--for _,v in ipairs(vim.api.nvim_list_wins()) do
+--vim.wo[v].statusline=' '
+--end
+--if vim.api.nvim_get_current_win()==win then return end
+--local s,maxwidth=pcall(vim.api.nvim_win_get_width,win)
+--if not s then maxwidth=vim.o.columns end
+--vim.api.nvim_buf_set_lines(buf,0,-1,false,{vim.api.nvim_eval_statusline(statusline,{maxwidth=maxwidth}).str})
+--local col=0
+--for _,v in ipairs(M.get_statusline({maxwidth=maxwidth},statusline)) do
+--vim.highlight.range(buf,vim.api.nvim_create_namespace(''),v[2],{0,col},{0,col+#v[1]})
+--col=col+#v[1]
+--end
+--end)
+--vim.api.nvim_create_autocmd('OptionSet',{pattern='statusline',callback=update})
+--update()
+--end
+function M.clear()
+    for _,winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        vim.wo[winid].statusline=' '
     end
-    return out
 end
-function M.replace_statusline()
-    if vim.o.laststatus~=1  then
-        error('\nWill not work if laststatus is not 1\nNote: if you change laststatus while it is active then it may break')
-        return
-    end
-    vim.o.laststatus=1
-    local buf=vim.api.nvim_create_buf(false,true)
-    local win=vim.api.nvim_open_win(buf,false,{
-        relative='win',
-        row=vim.o.lines-1,col=1,
-        width=vim.o.columns,height=1,
-        style='minimal'
+function M.run()
+    M.au_group=vim.api.nvim_create_augroup('small_statusbuf',{})
+    vim.api.nvim_set_decoration_provider(M.ns,{on_start=function ()
+        M.clear()
+    end})
+    vim.api.nvim_create_autocmd('ModeChanged',{
+        group=M.au_group,
+        callback=function ()
+            M.clear()
+        end
     })
-    local update=vim.schedule_wrap(function ()
-        local statusline=vim.o.statusline
-        if vim.o.statusline==' ' then return end
-        vim.o.statusline=' '
-        for _,v in ipairs(vim.api.nvim_list_wins()) do
-            vim.wo[v].statusline=' '
-        end
-        if vim.api.nvim_get_current_win()==win then return end
-        local s,maxwidth=pcall(vim.api.nvim_win_get_width,win)
-        if not s then maxwidth=vim.o.columns end
-        vim.api.nvim_buf_set_lines(buf,0,-1,false,{vim.api.nvim_eval_statusline(statusline,{maxwidth=maxwidth}).str})
-        local col=0
-        for _,v in ipairs(M.get_statusline({maxwidth=maxwidth},statusline)) do
-            vim.highlight.range(buf,vim.api.nvim_create_namespace(''),v[2],{0,col},{0,col+#v[1]})
-            col=col+#v[1]
-        end
-    end)
-    vim.api.nvim_create_autocmd('OptionSet',{pattern='statusline',callback=update})
-    update()
 end
 if vim.dev then
-    vim.o.laststatus=1
-    M.replace_statusline()
+    M.run()
 end
 return M
