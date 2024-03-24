27b6f5a271ce8b25720e509540e089018edba818
altermo <107814000+altermo@users.noreply.github.com>
2024-03-24 12:42:31 +0100

    M  lua/small/.not_done/statbuf.lua

--- lua/small/.not_done/statbuf.lua
+++ lua/small/.not_done/statbuf.lua
@@ -5,13 +5,12 @@ function M.update(win)
     if not lwin then return end
     local buf=vim.api.nvim_win_get_buf(lwin)
     vim.api.nvim_win_set_config(lwin,{
-        relative='win',
+        relative='win',win=win,
         row=conf.height,
-        col=1,
-        height=1,
-        width=conf.width-2,
+        col=1,height=1,
+        width=conf.width,
     })
-    local s=vim.api.nvim_eval_statusline(vim.wo[win].statusline or vim.o.statusline,{highlights=true,maxwidth=conf.width-2})
+    local s=vim.api.nvim_eval_statusline(vim.wo[win].statusline or vim.o.statusline,{highlights=true,maxwidth=conf.width})
     vim.api.nvim_buf_set_lines(buf,0,-1,false,{s.str})
     local col=0
     for k,v in ipairs(s.highlights) do
@@ -20,9 +19,14 @@ function M.update(win)
         col=col+#s.str:sub(v.start+1,next.start)
     end
 end
+function M.win_remove_status(win)
+    local lwin=vim.w[win].small_statbuf_win
+    if not lwin then return end
+    vim.api.nvim_win_close(lwin,true)
+end
 function M.win_create_status(win)
     local conf=vim.api.nvim_win_get_config(win)
-    if conf.relative~='' then return end
+    if conf.relative~='' then return true end
     local buf=vim.api.nvim_create_buf(false,true)
     vim.bo[buf].bufhidden='wipe'
     vim.w[win].small_statbuf_win=vim.api.nvim_open_win(buf,false,{
@@ -35,10 +39,20 @@ end
 function M.setup()
     vim.api.nvim_create_autocmd('WinNew',{callback=function ()
         M.win_create_status(vim.api.nvim_get_current_win())
+    end,group=M.group})
+    vim.api.nvim_create_autocmd('WinResized',{callback=function ()
         for _,win in pairs(vim.api.nvim_list_wins()) do
             M.update(win)
         end
     end,group=M.group})
+    vim.api.nvim_create_autocmd('WinClosed',{callback=function (ev)
+        M.win_remove_status(tonumber(ev.file))
+    end,group=M.group})
+    vim.api.nvim_create_autocmd('OptionSet',{callback=function ()
+        for _,win in pairs(vim.api.nvim_list_wins()) do
+            M.update(win)
+        end
+    end,group=M.group,pattern='statusline',})
     for _,win in pairs(vim.api.nvim_list_wins()) do
         M.win_create_status(win)
     end
