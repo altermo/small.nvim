23e9dfdefc070255aabe0d5319ff8864ee3b6cf0
altermo <107814000+altermo@users.noreply.github.com>
2024-05-12 02:21:02 +0200

    M lua/small/highlight_selected.lua

--- lua/small/highlight_selected.lua
+++ lua/small/highlight_selected.lua
@@ -1,5 +1,5 @@
 local M={}
-M.prev_match=nil
+M.prev_match_win=nil
 function M.get_visual()
     local reg=vim.region(0,'v','.','',true)
     if vim.tbl_count(reg)>1 then return end
@@ -9,16 +9,22 @@ function M.get_visual()
     return vim.api.nvim_buf_get_lines(0,linenr,linenr+1,false)[1]:sub(start+1,fin)
 end
 function M.clear()
-    if not M.prev_match then return end
-    pcall(vim.fn.matchdelete,unpack(M.prev_match))
-    M.prev_match=nil
+    if not M.prev_match_win then return end
+    for k,v in pairs(M.prev_match_win) do
+        pcall(vim.fn.matchdelete,v,k)
+    end
+    M.prev_match_win=nil
 end
 function M.do_highlight()
     M.clear()
     local line=M.get_visual()
     if not line or vim.trim(line)=='' or #line<2 then return end
     if vim.fn.type(line)==10 then return end
-    M.prev_match={vim.fn.matchadd('Visual','\\M'..vim.fn.escape(line,'\\')),vim.api.nvim_get_current_win()}
+    M.prev_match_win={}
+    for _,win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        local id=vim.fn.matchadd('Visual','\\M'..vim.fn.escape(line,'\\'),100,-1,{window=win})
+        M.prev_match_win[win]=id
+    end
 end
 function M.setup()
     vim.api.nvim_create_augroup('hisel',{})
