bf88992acf447a5ecb4118f2bc41514d53d1c3a5
altermo <107814000+altermo@users.noreply.github.com>
2024-05-31 00:06:14 +0200

    M lua/small/reminder.lua

--- lua/small/reminder.lua
+++ lua/small/reminder.lua
@@ -1,4 +1,4 @@
-local M={conf={}}
+local M={conf={},ns=vim.api.nvim_create_namespace('small.reminder')}
 function M.parse_date(date)
     local reg='(%d%d%d%d)%-(%d%d)%-(%d%d) (%d%d):(%d%d)'
     if not date:match(reg) then date=date..' 00:00' end
@@ -9,11 +9,13 @@ function M.get_times()
     local times={}
     local reg1=[[^%s*[-+] %[ %]%s*(.*)%s%(@(%d%d%d%d%-%d%d%-%d%d %d%d:%d%d)%)]]
     local reg2=[[^%s*[-+] %[ %]%s*(.*)%s%(@(%d%d%d%d%-%d%d%-%d%d)%)]]
+    local row=1
     for i in io.lines(M.conf.path) do
         for _,reg in ipairs{reg1,reg2} do
             local doc,date=i:match(reg)
-            if doc then table.insert(times,{doc,M.parse_date(date),date}) end
+            if doc then table.insert(times,{doc,M.parse_date(date),date,row}) end
         end
+        row=row+1
     end
     table.sort(times,function(a,b) return os.time(a[2])<os.time(b[2]) end)
     return times
@@ -47,10 +49,60 @@ function M.notify_today()
         end
     end
 end
+function M.draw(buf)
+    vim.api.nvim_buf_clear_namespace(buf,M.ns,0,-1)
+    local times=M.get_times()
+    for _,v in ipairs(times) do
+        local text=''
+        local hl
+        if (os.time()>os.time(v[2]) and #v[3]==16) or ((M.next_day()-86400)>os.time(v[2])) then
+            hl='Error'
+            text='OVERDUE'
+        elseif #v[3]==16 and M.next_day()>os.time(v[2]) then --TODO: test later
+            hl='Character'
+            local t=os.date('*t')
+            local min=v[2].min-t.min
+            local h=v[2].hour-t.hour
+            if min<0 then min=60+min h=h-1 end
+            text='in '..h..'h '..min..'m'
+        elseif M.next_day()>os.time(v[2]) then
+            hl='Character'
+            text='TODAY'
+        else
+            hl='String'
+            local t=os.time(v[2])-os.time()+86400
+            if t>=31556926 then
+                text=text..math.floor(t/31556926)..'y '
+                t=t%31556926
+            end
+            if t>=2629743 then
+                text=text..math.floor(t/2629743)..'m '
+                t=t%2629743
+            end
+            if t>=604800 then
+                text=text..math.floor(t/604800)..'w '
+                t=t%604800
+            end
+            if t>=86400 then
+                text=text..math.floor(t/86400)..'d '
+                t=t%86400
+            end
+        end
+        vim.api.nvim_buf_set_extmark(buf,M.ns,v[4]-1,0,{virt_text={{'due: ','Comment'},{text,hl}}})
+    end
+end
 function M.setup()
     if not M.conf.path then error('conf: small.reminder.conf.path is not set') end
     vim.fn.timer_start(30000,vim.schedule_wrap(M.notify_timed),{['repeat']=-1})
     vim.defer_fn(M.notify_today,1000)
+    vim.api.nvim_create_autocmd('BufRead',{callback=function (ev)
+        if ev.file==M.conf.path then
+            M.draw(ev.buf)
+            vim.api.nvim_create_autocmd('TextChanged',{buffer=ev.buf,callback=function ()
+                M.draw(ev.buf)
+            end})
+        end
+    end})
 end
 function M.sidebar()
     local times=M.get_times()
