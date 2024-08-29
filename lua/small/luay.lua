88ab4e65a522a70e1d32896d0d6fe39d8cac5aad
altermo <107814000+altermo@users.noreply.github.com>
2024-08-29 10:12:37 +0200

    ;improve luay error msg

--- lua/small/luay.lua
+++ lua/small/luay.lua
@@ -3,20 +3,27 @@ function M.refresh()
     if not M.buf then return end
     if not vim.api.nvim_buf_is_valid(M.buf) then return end
     local lines=table.concat(vim.api.nvim_buf_get_lines(M.buf,0,-1,false),'\n')
-    local f,errmsg=loadstring(lines)
+    local f,errmsg=loadstring('\n'..lines)
     vim.api.nvim_buf_clear_namespace(M.buf,M.ns,0,-1)
-    if not f or errmsg then return end
+    if not f or errmsg then
+        if not errmsg then return end
+        errmsg:gsub('^.-:(.-): (.*)$',function (n,msg)
+            vim.api.nvim_buf_set_extmark(M.buf,M.ns,assert(tonumber(n))-2,0,{virt_text={{msg,'ErrorMsg'}}})
+        end)
+        return
+    end
     local output=vim.defaulttable(function () return {} end)
     setfenv(f,setmetatable({
         print=function (...)
             if select('#',...)==0 then return end
-            local line=debug.getinfo(2,'l').currentline-1
+            local line=debug.getinfo(2,'l').currentline-2
             if #output[line]>100 then return end
             vim.list_extend(output[line],vim.tbl_map(vim.inspect,{...}))
         end
     },{__index=_G}))
     if not xpcall(f,function (msg)
-        local line=debug.getinfo(3,'l').currentline-1
+        local line=debug.getinfo(2,'l').currentline-2
+        msg=msg:gsub('^.-:.-: ','')
         vim.api.nvim_buf_set_extmark(M.buf,M.ns,line,0,{virt_text={{'Error: '..msg,'ErrorMsg'}}})
     end) then return end
     for row,v in pairs(output) do
